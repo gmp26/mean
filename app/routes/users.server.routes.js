109f819f85fd74c299ffafc438ac533d5d8cf8f4
gmp26 <gmp26@cam.ac.uk>
2014-05-19 22:38:36 +0100

    added mean prefix to routes

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -6,41 +6,41 @@
 var passport = require('passport');
 
 module.exports = function(app) {
-	// User Routes
-	var users = require('../../app/controllers/users');
-	app.route('/users/me').get(users.me);
-	app.route('/users').put(users.update);
-	app.route('/users/password').post(users.changePassword);
-	app.route('/users/accounts').delete(users.removeOAuthProvider);
+    // User Routes
+    var users = require('../../app/controllers/users');
+    app.route('/mean/users/me').get(users.me);
+    app.route('/mean/users').put(users.update);
+    app.route('/mean/users/password').post(users.changePassword);
+    app.route('/mean/users/accounts').delete(users.removeOAuthProvider);
 
-	// Setting up the users api
-	app.route('/auth/signup').post(users.signup);
-	app.route('/auth/signin').post(users.signin);
-	app.route('/auth/signout').get(users.signout);
+    // Setting up the users api
+    app.route('/mean/auth/signup').post(users.signup);
+    app.route('/mean/auth/signin').post(users.signin);
+    app.route('/mean/auth/signout').get(users.signout);
 
-	// Setting the facebook oauth routes
-	app.route('/auth/facebook').get(passport.authenticate('facebook', {
-		scope: ['email']
-	}));
-	app.route('/auth/facebook/callback').get(users.oauthCallback('facebook'));
+    // Setting the facebook oauth routes
+    app.route('/mean/auth/facebook').get(passport.authenticate('facebook', {
+        scope: ['email']
+    }));
+    app.route('/mean/auth/facebook/callback').get(users.oauthCallback('facebook'));
 
-	// Setting the twitter oauth routes
-	app.route('/auth/twitter').get(passport.authenticate('twitter'));
-	app.route('/auth/twitter/callback').get(users.oauthCallback('twitter'));
+    // Setting the twitter oauth routes
+    app.route('/mean/auth/twitter').get(passport.authenticate('twitter'));
+    app.route('/mean/auth/twitter/callback').get(users.oauthCallback('twitter'));
 
-	// Setting the google oauth routes
-	app.route('/auth/google').get(passport.authenticate('google', {
-		scope: [
-			'https://www.googleapis.com/auth/userinfo.profile',
-			'https://www.googleapis.com/auth/userinfo.email'
-		]
-	}));
-	app.route('/auth/google/callback').get(users.oauthCallback('google'));
+    // Setting the google oauth routes
+    app.route('/mean/auth/google').get(passport.authenticate('google', {
+        scope: [
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email'
+        ]
+    }));
+    app.route('/mean/auth/google/callback').get(users.oauthCallback('google'));
 
-	// Setting the linkedin oauth routes
-	app.route('/auth/linkedin').get(passport.authenticate('linkedin'));
-	app.route('/auth/linkedin/callback').get(users.oauthCallback('linkedin'));
+    // Setting the linkedin oauth routes
+    app.route('/mean/auth/linkedin').get(passport.authenticate('linkedin'));
+    app.route('/mean/auth/linkedin/callback').get(users.oauthCallback('linkedin'));
 
-	// Finish by binding the user middleware
-	app.param('userId', users.userByID);
+    // Finish by binding the user middleware
+    app.param('userId', users.userByID);
 };
