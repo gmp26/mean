ececd1da2e07b1e731252d1d4b0a9a44d2612110
gmp26 <gmp26@cam.ac.uk>
2014-05-20 22:14:05 +0100

    added school fields

--- app/models/user.server.model.js
+++ app/models/user.server.model.js
@@ -4,136 +4,190 @@
  * Module dependencies.
  */
 var mongoose = require('mongoose'),
-	Schema = mongoose.Schema,
-	crypto = require('crypto');
+    Schema = mongoose.Schema,
+    crypto = require('crypto');
 
 /**
  * A Validation function for local strategy properties
  */
 var validateLocalStrategyProperty = function(property) {
-	return ((this.provider !== 'local' && !this.updated) || property.length);
+    return ((this.provider !== 'local' && !this.updated) || property.length);
 };
+
+var validateString40 = function(property) {
+    return ((this.provider !== 'local' && !this.updated) || property.length <= 40);
+}
+
+var validateString20 = function(property) {
+    return ((this.provider !== 'local' && !this.updated) || property.length <= 20);
+}
+
+var validateString10 = function(property) {
+    return ((this.provider !== 'local' && !this.updated) || property.length <= 10);
+}
 
 /**
  * A Validation function for local strategy password
  */
 var validateLocalStrategyPassword = function(password) {
-	return (this.provider !== 'local' || (password && password.length > 6));
+    return (this.provider !== 'local' || (password && password.length > 6));
 };
 
 /**
  * User Schema
  */
 var UserSchema = new Schema({
-	firstName: {
-		type: String,
-		trim: true,
-		default: '',
-		validate: [validateLocalStrategyProperty, 'Please fill in your first name']
-	},
-	lastName: {
-		type: String,
-		trim: true,
-		default: '',
-		validate: [validateLocalStrategyProperty, 'Please fill in your last name']
-	},
-	displayName: {
-		type: String,
-		trim: true
-	},
-	email: {
-		type: String,
-		trim: true,
-		default: '',
-		validate: [validateLocalStrategyProperty, 'Please fill in your email'],
-		match: [/.+\@.+\..+/, 'Please fill a valid email address']
-	},
-	username: {
-		type: String,
-		unique: true,
-		required: 'Please fill in a username',
-		trim: true
-	},
-	password: {
-		type: String,
-		default: '',
-		validate: [validateLocalStrategyPassword, 'Password should be longer']
-	},
-	salt: {
-		type: String
-	},
-	provider: {
-		type: String,
-		required: 'Provider is required'
-	},
-	providerData: {},
-	additionalProvidersData: {},
-	roles: {
-		type: [{
-			type: String,
-			enum: ['user', 'admin']
-		}],
-		default: ['user']
-	},
-	updated: {
-		type: Date
-	},
-	created: {
-		type: Date,
-		default: Date.now
-	}
+    firstName: {
+        type: String,
+        trim: true,
+        default: '',
+        validate: [validateLocalStrategyProperty, 'Please fill in your first name']
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        default: '',
+        validate: [validateLocalStrategyProperty, 'Please fill in your last name']
+    },
+    displayName: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true,
+        default: '',
+        validate: [validateLocalStrategyProperty, 'Please fill in your email'],
+        match: [/.+\@.+\..+/, 'Please fill a valid email address']
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: 'Please fill in an email',
+        trim: true
+    },
+    password: {
+        type: String,
+        default: '',
+        validate: [validateLocalStrategyPassword, 'Password should be longer']
+    },
+    affiliated: {
+        type: Boolean,
+        default: false
+    },
+    schoolurn: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    schoolname: {
+        type: String,
+        default: '',
+        trim: true,
+        validate: [validateString40, "school name too long"]
+    },
+    schooladdr1: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    schooladdr2: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    schooladdr3: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    schooltown: {
+        type: String,
+        default: '',
+        trim: true,
+        validate: [validateString20, "school town too long"]
+    },
+    schoolpostCode: {
+        type: String,
+        default: '',
+        trim: true,
+        validate: [validateString10, "school post code too long"]
+    },
+    salt: {
+        type: String
+    },
+    provider: {
+        type: String,
+        required: 'Provider is required'
+    },
+    providerData: {},
+    additionalProvidersData: {},
+    roles: {
+        type: [{
+            type: String,
+            enum: ['user', 'admin']
+        }],
+        default: ['user']
+    },
+    updated: {
+        type: Date
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 /**
  * Hook a pre save method to hash the password
  */
 UserSchema.pre('save', function(next) {
-	if (this.password && this.password.length > 6) {
-		this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
-		this.password = this.hashPassword(this.password);
-	}
+    if (this.password && this.password.length > 6) {
+        this.salt = new Buffer(crypto.randomBytes(16).toString('base64'), 'base64');
+        this.password = this.hashPassword(this.password);
+    }
 
-	next();
+    next();
 });
 
 /**
  * Create instance method for hashing a password
  */
 UserSchema.methods.hashPassword = function(password) {
-	if (this.salt && password) {
-		return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
-	} else {
-		return password;
-	}
+    if (this.salt && password) {
+        return crypto.pbkdf2Sync(password, this.salt, 10000, 64).toString('base64');
+    } else {
+        return password;
+    }
 };
 
 /**
  * Create instance method for authenticating user
  */
 UserSchema.methods.authenticate = function(password) {
-	return this.password === this.hashPassword(password);
+    return this.password === this.hashPassword(password);
 };
 
 /**
  * Find possible not used username
  */
 UserSchema.statics.findUniqueUsername = function(username, suffix, callback) {
-	var _this = this;
-	var possibleUsername = username + (suffix || '');
+    var _this = this;
+    var possibleUsername = username + (suffix || '');
 
-	_this.findOne({
-		username: possibleUsername
-	}, function(err, user) {
-		if (!err) {
-			if (!user) {
-				callback(possibleUsername);
-			} else {
-				return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
-			}
-		} else {
-			callback(null);
-		}
-	});
+    _this.findOne({
+        username: possibleUsername
+    }, function(err, user) {
+        if (!err) {
+            if (!user) {
+                callback(possibleUsername);
+            } else {
+                return _this.findUniqueUsername(username, (suffix || 0) + 1, callback);
+            }
+        } else {
+            callback(null);
+        }
+    });
 };
 
 mongoose.model('User', UserSchema);
