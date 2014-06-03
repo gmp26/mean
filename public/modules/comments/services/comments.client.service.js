3d765133f59094abc7d6584c7354f512a827372f
gmp26 <gmp26@cam.ac.uk>
2014-06-03 10:37:08 +0100

    change

--- public/modules/comments/services/comments.client.service.js
+++ public/modules/comments/services/comments.client.service.js
@@ -3,7 +3,7 @@
 //Comments service used for communicating with the comments REST endpoints
 angular.module('comments').factory('Comments', ['$resource',
     function($resource) {
-        return $resource('comments/:commentId', {
+        return $resource('/mean/comments/:commentId', {
             commentId: '@_id'
         }, {
             update: {
