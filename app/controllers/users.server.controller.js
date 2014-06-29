6e29139606971e6c75e640b7446557663a5bed49
gmp26 <gmp26@cam.ac.uk>
2014-06-29 23:32:29 +0100

    provisional one-time auth. Should probably move this code to LocalStrategy

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -12,6 +12,18 @@ var mongoose = require('mongoose'),
     debug = require('debug')('users');
 
 
+var oneTimes = {};
+
+function clearOneTime(email) {
+    debug('deleting one time password ' + email);
+
+    // cancel the timeout
+    oneTimes[email][0]();
+
+    // delete the record from the oneTimes object
+    delete oneTimes[email];
+}
+
 /**
  * Get the error message from error object
  */
@@ -88,6 +100,44 @@ exports.signup = function(req, res) {
  */
 exports.signin = function(req, res, next) {
     debug('signin request');
+    // First check whether this is a one-time sign-in
+    debug('req.body = ' + util.inspect(req.body));
+
+    if (req.body) {
+        var email = req.body.email;
+        var password = req.body.password;
+        if (oneTimes) {
+            var ot = oneTimes[email];
+            if (ot && ot[1] === password) {
+
+                // clear the oneTime password timeout
+                ot[0]();
+                delete oneTimes[email];
+
+                // fetch user details
+                User.findOne({
+                    email: email
+                }).exec(function(err, user) {
+                    if (err) {
+                        debug(err);
+                        res.send(400, {
+                            message: 'Lookup failure on ' + email
+                        });
+                    } else {
+
+                        // We need to set the session cookie so user
+                        // can be redirected to settings page.
+
+                        res.json(200, {
+                            user: user
+                        });
+                    }
+                });
+            }
+        }
+    }
+
+    // otherwise drop through to normal authenticate
     passport.authenticate('local', function(err, user, info) {
         if (err) {
             debug('signin err = ' + err);
@@ -201,14 +251,6 @@ exports.update = function(req, res) {
     }
 };
 
-
-var oneTimes = {};
-
-function clearOneTime(email) {
-    debug('deleting one time password ' + email);
-    delete oneTimes[email];
-}
-
 // This is an example that works on the server:
 
 var smtpTransport = nodemailer.createTransport('SMTP', {
@@ -222,15 +264,17 @@ var smtpTransport = nodemailer.createTransport('SMTP', {
 });
 
 var mailOptions = {
-    from: '<gmp26@cam.ac.uk>', // sender address
-    to: 'gmp26@cam.ac.uk', // list of receivers
-    subject: 'CMEP Password Reset Request (testing)', // Subject line
+    from: '<gmp26@cam.ac.uk>',
+    to: 'gmp26@cam.ac.uk',
+    subject: 'CMEP Password Reset Request (testing)',
     html: '<p>We received a password reset request for your CMEP account.</p>' +
-        '<p>A new password has been generated for you which is good for one sign-in only. ' +
+        '<p>A new password has been generated for you which is good ' +
+        'for one sign-in only within the next hour. ' +
         'Your one-time password is</p>' +
         '<div style="font-weight:bold;font-size:1.8em">password</div>' +
         '<p>Please sign in with this password. ' +
-        'You will then be redirected to your settings page where you can choose a new password</p>'
+        'You will then be redirected to your settings page where you can choose a new password</p>' +
+        '<p>If this password has already expired, you will be given an opportunity to request another.</p>'
 };
 
 /**
@@ -245,12 +289,12 @@ exports.resetPassword = function(req, res) {
         }).exec(function(err, user) {
             if (err) {
                 debug(err);
-                res.send(400, {
+                return res.send(400, {
                     message: 'Lookup failure on ' + resetEmail
                 });
             } else {
                 // generate one-time password
-                var oneTime = 'password';
+                var oneTime = 'password ';
 
                 // start timeout on password
                 var timeout = setTimeout(clearOneTime, 1000 * 3600, resetEmail);
@@ -264,25 +308,25 @@ exports.resetPassword = function(req, res) {
                     if (err) {
                         console.log(err);
                         res.send(400, {
-                            message: 'unable to send email'
+                            message: 'unable to send email '
                         });
                     } else {
                         console.log('Message sent: ' + response.message);
                         // simple success displays instructions to user
-                        res.send(200);
+                        return res.json(200, {
+                            message: 'email sent '
+                        });
                     }
                     // if you don't want to use this transport object anymore, uncomment following line
                     //smtpTransport.close(); // shut down the connection pool, no more messages
                 });
-
-
             }
         });
+    } else {
+        return res.json(400, {
+            message: 'no email in request'
+        });
     }
-
-    return res.json(200, {
-        message: 'email sent'
-    });
 };
 
 /**
